6642146d84561202c0484d8d37588684307c711f
Tibold Kandrai <tibold@kandrai.eu>
2014-07-07 17:46:59 +0200

    Fixed the compiler not to fail on warnings.

--- src/Core/RazorEngine.Core/Compilation/DirectCompilerServiceBase.cs
+++ src/Core/RazorEngine.Core/Compilation/DirectCompilerServiceBase.cs
@@ -102,7 +102,7 @@
             var result = Compile(context);
             var compileResult = result.Item1;
 
-            if (compileResult.Errors != null && compileResult.Errors.Count > 0)
+            if (compileResult.Errors != null && compileResult.Errors.Count > 0 && compileResult.Errors.OfType<CompilerError>().Any(error => !error.IsWarning))
                 throw new TemplateCompilationException(compileResult.Errors, result.Item2, context.TemplateContent);
 
             return Tuple.Create(
